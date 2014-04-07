7a8ca460be466ca52cceb030f5b02d3661ac8fcc
Sami Purmonen <purmonen@kth.se>
2014-04-07 16:05:48 +0200

    Cleaned up controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,97 +3,80 @@
 /* Controllers */
 
 var controllers = angular.module('myApp.controllers', []);
+controllers.controller('ProjectCtrl', function() {});
 
-controllers.controller('ProjectCtrl', [function() {}]);
+var WallCtrl = function($scope, $sce, $route, $http, $cookieStore, $timeout, $location, Post) {
+	$scope.password = $cookieStore.get('password');
+	$scope.posts = Post;
+	var lastSaveIndex = 0;
 
-controllers.controller('WallCtrl', ['$scope', '$sce', '$route', '$http', '$cookieStore', '$timeout', '$location', 'Post',
-	function($scope, $sce, $route, $http, $cookieStore, $timeout, $location, Post) {
-		$scope.password = $cookieStore.get('password');
-		$scope.errorMessage = '';
-		$scope.successMessage = '';
+	$scope.cancel = function() {
+		$location.path('/wall');
+	}
 
-		$scope.posts = Post;
-		var lastSaveIndex = 0;
+	$scope.html = function(html) {
+		return $sce.trustAsHtml(html);
+	}
 
-		$scope.cancel = function() {
+	$scope.edit = function(post) {
+		$location.path('/post/' + Post.get().indexOf(post));
+	}
+};
+WallCtrl.$inject = ['$scope', '$sce', '$route', '$http', '$cookieStore', '$timeout', '$location', 'Post'];
+controllers.controller('WallCtrl', WallCtrl);
+
+var PostCtrl = function($scope, $sce, $route, $http, $cookieStore, $location, $routeParams, Post) {
+	$scope.password = $cookieStore.get('password');
+
+	var lastSaveIndex = 0;
+	$scope.post = {
+		content: '',
+		backgroundColor: '#ffffff',
+		color: '#000000',
+		fontSize: '30',
+		fontFamily: 'fantasy',
+		timestamp: Date.now()
+	};
+
+	$scope.isNew = true;
+	var editIndex;
+
+	if ($routeParams.index) {
+		editIndex = parseInt($routeParams.index);
+		$scope.post = angular.copy(Post.get()[editIndex]);
+		$scope.isNew = false;
+	}
+
+	$scope.add = function() {
+		if ($scope.isNew) {
+			Post.add($scope.post);
+		} else {
+			Post.edit($scope.post, editIndex);
+		}
+		$location.path('/wall');
+	};
+
+	$scope.cancel = function() {
+		$location.path('/wall');
+	}
+
+	$scope.html = $sce.trustAsHtml;
+
+	$scope.delete = function() {
+		Post.delete(editIndex);
+		$location.path('/wall');
+	}
+
+	$scope.save = function() {
+		$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.password);
+		Post.save(function() {
+			$cookieStore.put('password', $scope.password);
 			$location.path('/wall');
-		}
-
-		$scope.html = function(html) {
-			return $sce.trustAsHtml(html);
-		}
-
-		$scope.edit = function(post) {
-			$location.path('/post/' + Post.get().indexOf(post));
-		}
-		/*
-		window.onbeforeunload = function() {
-			if ($scope.changeManager.getIndex() !== lastSaveIndex) {
-				return 'You have unsaved changes. Are you sure you want to leave?';
-			}
-		}
-		*/
-	}]);
-
-controllers.controller('PostCtrl', ['$scope', '$sce', '$route', '$http', '$cookieStore', '$location', '$routeParams', 'Post',
-	function($scope, $sce, $route, $http, $cookieStore, $location, $routeParams, Post) {
-		$scope.password = $cookieStore.get('password');
-
-		var lastSaveIndex = 0;
-		$scope.post = {
-			content: '',
-			backgroundColor: '#ffffff',
-			color: '#000000',
-			fontSize: '30',
-			fontFamily: 'fantasy',
-			timestamp: Date.now()
-		}
-
-		$scope.isNew = true;
-		var editIndex;
-
-		if ($routeParams.index) {
-			editIndex = parseInt($routeParams.index);
-			$scope.post = angular.copy(Post.get()[editIndex]);
-			$scope.isNew = false;
-		}
-
-		$scope.add = function() {
-			if ($scope.isNew) {
-				Post.add($scope.post);
-			} else {
-				Post.edit($scope.post, editIndex);
-			}
-			$location.path('/wall');
-		};
-
-		$scope.cancel = function() {
-			$location.path('/wall');
-		}
-
-		$scope.html = $sce.trustAsHtml;
-
-		$scope.delete = function() {
-			Post.delete(editIndex);
-			$location.path('/wall');
-		}
-
-		$scope.save = function() {
-			$http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.password);
-			Post.save(function() {
-				$cookieStore.put('password', $scope.password);
-				$location.path('/wall');
-			}, function() {
-				$scope.errorMessage = 'Wrong password';
-				$scope.password = '';
-			});
-		}
-
-		/*
-		window.onbeforeunload = function() {
-			if ($scope.changeManager.getIndex() !== lastSaveIndex) {
-				return 'You have unsaved changes. Are you sure you want to leave?';
-			}
-		}
-		*/
-	}]);
+		}, function() {
+			$scope.errorMessage = 'Wrong password';
+			$scope.password = '';
+		});
+	}
+};
+PostCtrl.$inject = ['$scope', '$sce', '$route', '$http', '$cookieStore', '$location', '$routeParams', 'Post'];
+controllers.controller('PostCtrl', PostCtrl);
